b64f7f1099209e0a4ad880f3fc58a36b5e10bd6a
vinegarboy <necogusa398@gmail.com>
2024-04-12 11:58:20 +0900

    Slackでのbotの操作部分の基礎を設置

--- Huggingface.py
+++ Huggingface.py
@@ -1,44 +1,43 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import sys
 
-# Hugging FaceのモデルIDを指定
-model_id = "CohereForAI/c4ai-command-r-plus"
 
-# 起動引数を取得
-args = sys.argv
+class UsingHuggingface:
 
-# 起動引数の数に応じて変数を設定
-if len(args) == 3:
-    # モデルIDを指定する場合
-    model_id = args[2]
-elif len(args) == 2:
-    # メッセージのみの指定
-    pass
-else:
-    # 引数が不正な場合
-    print("引数が不正です")
-    sys.exit()
+    # Hugging FaceのモデルIDを指定
+    model_id = "CohereForAI/c4ai-command-r-plus"
+    
+    # モデルとトークナイザーを初期宣言
+    tokenizer = None
+    model = None
 
-# 指定されたモデルIDを使用してトークナイザーをロード
-tokenizer = AutoTokenizer.from_pretrained(model_id)
-# 指定されたモデルIDを使用して生成モデルをロード
-model = AutoModelForCausalLM.from_pretrained(model_id)
+    def __init__(self,models = None):
+        # 起動引数の数に応じて変数を設定
+        if models != None:
+            # モデルIDを指定する場合
+            model_id = models
 
-# チャット形式のメッセージをリストとして定義
-messages = [{"role": "user", "content": args[1]}]
-# メッセージをトークナイザーに渡し、トークン化された入力IDを生成
-input_ids = tokenizer.apply_chat_template(messages, tokenize=True, add_generation_prompt=True, return_tensors="pt")
+        # 指定されたモデルIDを使用してトークナイザーをロード
+        tokenizer = AutoTokenizer.from_pretrained(model_id)
+        # 指定されたモデルIDを使用して生成モデルをロード
+        model = AutoModelForCausalLM.from_pretrained(model_id)
 
-# 入力IDに基づいて新しいテキストを生成
-gen_tokens = model.generate(
-    input_ids, 
-    max_new_tokens=100, 
-    do_sample=True, 
-    temperature=0.3,
-    )
+    def talk(content:str):
+        # チャット形式のメッセージをリストとして定義
+        messages = [{"role": "user", "content": content}]
+        # メッセージをトークナイザーに渡し、トークン化された入力IDを生成
+        input_ids = tokenizer.apply_chat_template(messages, tokenize=True, add_generation_prompt=True, return_tensors="pt")
 
-# 生成されたトークンをデコードして、人間が読めるテキストに変換
-gen_text = tokenizer.decode(gen_tokens[0])
+        # 入力IDに基づいて新しいテキストを生成
+        gen_tokens = model.generate(
+            input_ids, 
+            max_new_tokens=100, 
+            do_sample=True, 
+            temperature=0.3,
+        )
 
-# 生成されたテキストを出力
-print(gen_text)
+        # 生成されたトークンをデコードして、人間が読めるテキストに変換
+        gen_text = tokenizer.decode(gen_tokens[0])
+
+        # 生成されたテキストを出力
+        return gen_text
